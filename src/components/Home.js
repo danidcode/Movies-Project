fcae7ba63380eabe671e60602cb93acf9a31e0fd
danidcode <danieldenizvega@gmail.com>
2022-07-23 23:02:54 +0200

    Building cards for  movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,16 @@
 import React, { useContext, useEffect, useState } from "react";
 import movieContext from '../context/Movies/MovieContext';
-
+import MovieCard from './Movies/MovieCard';
 const Home = () => {
   const { movies, getMovies } = useContext(movieContext);
 
   useEffect(() => {
     let movies = getMovies();
-    console.log(movies);
   }, []);
   return (
       <div className='homeContainer'>
-          
+          {movies.map((movie) => <MovieCard key={movie.id} {...movie}/>)}
+
       </div>
   );
 }
--- src/components/Movies/MovieCard.js
+++ src/components/Movies/MovieCard.js
@@ -1,8 +1,24 @@
 import React from 'react';
-
-const MovieCard = () => {
+const IMG_API = "https://image.tmdb.org/t/p/w500";
+const MovieCard = ({title, poster_path, release_date, popularity}) => {
   return (
-      <div className='cardContainer'>
+      <div className='cards'>
+        <div className="card-content">
+  
+  <div className="movie">
+    <a href="#" className="movie_card-link"></a>
+    <img src={IMG_API+poster_path} alt="" className="movie_image"/>
+    <div className="movie_text-content">
+      <h2 className="movie_title">Amazing First Title</h2>
+      <div className="movie_post-date">Jan 29, 2018</div>
+      <div className="movie_details-content">
+        <p className="movie_excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est pariatur nemo tempore repellat? Ullam sed officia iure architecto deserunt distinctio, pariatur&hellip;</p>
+        <a href="#" className="movie_read-more">Read more</a>
+      </div>
+    </div>
+  </div>
+
+</div>
       </div>
   );
 }

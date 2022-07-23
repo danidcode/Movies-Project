6ef93821e9b2c27dcba69e282141a7e2fde07cd0
danidcode <danieldenizvega@gmail.com>
2022-07-24 01:14:38 +0200

    Cards effects

--- src/components/Movies/MovieCard.js
+++ src/components/Movies/MovieCard.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 const IMG_API = "https://image.tmdb.org/t/p/w500";
 const MovieCard = ({title, poster_path, release_date, popularity}) => {
   return (
+    
       <div className='cards'>
         <div className="card-content">
   
@@ -9,11 +11,10 @@ const MovieCard = ({title, poster_path, release_date, popularity}) => {
     <a href="#" className="movie_card-link"></a>
     <img src={IMG_API+poster_path} alt="" className="movie_image"/>
     <div className="movie_text-content">
-      <h2 className="movie_title">Amazing First Title</h2>
-      <div className="movie_post-date">Jan 29, 2018</div>
+      <h2 className="movie_title">{title}</h2>
+      <div className="movie_post-date">{release_date}</div>
       <div className="movie_details-content">
-        <p className="movie_excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est pariatur nemo tempore repellat? Ullam sed officia iure architecto deserunt distinctio, pariatur&hellip;</p>
-        <a href="#" className="movie_read-more">Read more</a>
+      <Link to="/"> View more </Link>
       </div>
     </div>
   </div>

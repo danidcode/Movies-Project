834f7c8630123eeab3bd60bd3a4cfa938dbec415
danidcode <danieldenizvega@gmail.com>
2022-07-24 16:51:22 +0200

    Changing the logic in Details to show the movie and starting to design the details component

--- src/components/Movies/Details.js
+++ src/components/Movies/Details.js
@@ -1,22 +1,34 @@
 import React, { useContext, useEffect, useState } from "react";
 import movieContext from '../../context/Movies/MovieContext';
 import {useParams} from "react-router-dom";
+import Spinner from 'react-bootstrap/Spinner';
 const IMG_API = "https://image.tmdb.org/t/p/w500";
 
+
 const Details = () => {
+
   const { getOneMovie, selectedMovie } = useContext(movieContext);
+  const [isLoading, setLoading] = useState(true);
+
   //Getting the id from the url
   const { id } = useParams();
   useEffect(() => {
-    getOneMovie(id)
+    //While getOneMovie is retrieving the data from the API, The page will be loading
+    getOneMovie(id).then(() => setLoading(false));
   }, [])
   
   return (
+    
       <div className='detailsContainer'>
+        
          {/* Conditional rendering because selectedMovie will be undefined until the request to the API ends */}
-        {selectedMovie ?  <img src={IMG_API+selectedMovie.poster_path} alt="" className="movie_image"/>
-       : null }
+       {isLoading ? <Spinner animation="border" variant="secondary" /> :    
        
+       <div className="details-movie_image_div">
+       <img src={IMG_API+selectedMovie.poster_path} alt="" className="details-movie_image"/>
+       </div>
+       
+       }
       </div>
   );
 }
